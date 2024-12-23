9f85d7dfb33a78745abdceaf31798144ad796606
ammyy9908 <sumit@kodnest.com>
2024-12-23 20:46:14 +0530

    setup commands added

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#install maven
+sudo apt install maven
+
+#install openjdk
+sudo apt install openjdk-17-jdk
+
+#install cdm
+npm install -g cdm
+
+#install cdm-extension
+cdm-extension install
